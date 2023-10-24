56cda0fe0e29f215dfd981cd84b9ec0c27776d12
Lukas . <lukas.@Lukass-MacBook-Air.local>
2023-10-24 14:37:29 -0600

    added work done so far

---
+++ duckJumpGame/app/src/main/java/com/example/duckjumpgame/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.duckjumpgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    //This method will be called when the button is clicked
+    public void startGame(View view){
+        // Start game when the button is clicked
+        Intent intent = new Intent(this, gameManager.class);
+        startActivity(intent);
+    }
+    public void openCredits(View myView){
+        TextView mainText = findViewById(R.id.mainText);
+        Button playButton = findViewById(R.id.playButton);
+        Button creditButton = findViewById(R.id.creditButton);
+        mainText.setText("Created by: Cole Edan and Lukas");
+        mainText.setTextSize(30);
+        creditButton.setOnClickListener(this::backToMain);
+
+        creditButton.setText("Back To Menu");
+        playButton.setVisibility(View.INVISIBLE);
+        playButton.setEnabled(false);
+    }
+    public void backToMain(View myView){
+        TextView mainText = findViewById(R.id.mainText);
+        Button playButton = findViewById(R.id.playButton);
+        Button creditButton = findViewById(R.id.creditButton);
+        mainText.setText("Duck Jump");
+        mainText.setTextSize(50);
+        creditButton.setOnClickListener(this::openCredits);
+
+        creditButton.setText("Credits");
+        playButton.setVisibility(View.VISIBLE);
+        playButton.setEnabled(true);
+    }
+
+}
---
+++ duckJumpGame/app/src/main/java/com/example/duckjumpgame/winPage.java
@@ -0,0 +1,23 @@
+package com.example.duckjumpgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class winPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win_page);
+
+    }
+
+    public void enterMainMenu(View view){
+        //Go back to main menu when the button is clicked
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
